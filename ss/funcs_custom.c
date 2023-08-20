9bebc905525bde84174dd60f9432af4793b4e80b
B-Amal <amalboughzala1@gmail.com>
2023-08-20 09:21:23 -0700

    task 1-8

---
+++ ss/funcs_custom.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * _which - locates the path of a command
+ * @command: command
+ *
+ * Return: path to executable file if found
+ */
+char *_which(char *command)
+{
+	char *path, *path_copy, *path_token, *file_path;
+	int command_length, directory_length;
+
+	if (access(command, X_OK) == 0)
+		return (strdup(command));
+	path = getenv("PATH");
+	if (path)
+	{
+		path_copy = strdup(path);
+		if (path_copy == NULL)
+			return (NULL);
+		command_length = strlen(command);
+		path_token = strtok(path_copy, ":");
+		while (path_token != NULL)
+		{
+			directory_length = strlen(path_token);
+			file_path = (char *)malloc(command_length + directory_length + 2);
+			if (file_path == NULL)
+			{
+				free(path_copy);
+				return (NULL);
+			}
+			strcpy(file_path, path_token);
+			strcat(file_path, "/");
+			strcat(file_path, command);
+			if (access(file_path, X_OK) == 0)
+			{
+				free(path_copy);
+				return (file_path);
+			}
+			else
+			{
+				free(file_path);
+				path_token = strtok(NULL, ":");
+			}
+		}
+		free(path_copy);
+	}
+	return (NULL);
+}
---
+++ ss/funcs_mem.c
@@ -0,0 +1,77 @@
+#include "shell.h"
+
+/**
+ * free_array - frees an array
+ * @argv: array of tokens
+ *
+ * Return: void
+ */
+void free_array(char **argv)
+{
+	int i = 0;
+
+	while (argv[i] != NULL)
+	{
+		free(argv[i]);
+		i++;
+	}
+	free(argv);
+}
+/**
+ * _memcpy - copy m bytes from m1 to m2
+ * @dest: 1st memory block
+ * @src: 2nd memory block
+ * @m: number of bytes to copy
+ *
+ * Return: pointer to memory block
+ */
+char *_memcpy(char *dest, char *src, unsigned int m)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < m)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to previous memory block
+ * @old_size: size in bytes of previous memory block
+ * @new_size: size in bytes of new memory block
+ *
+ * Return: pointer to the new allocated memory, NULL on failure
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int min;
+	void *new_ptr;
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size < old_size)
+		min = new_size;
+	else
+		min = old_size;
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	new_ptr = _memcpy(new_ptr, ptr, min);
+	free(ptr);
+	return (new_ptr);
+}
