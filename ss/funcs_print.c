acbb3ae44305ff8fe93f01e6a8f651055836f8fb
B-Amal <amalboughzala1@gmail.com>
2023-08-22 00:53:00 -0700

    Add file for err msgs

--- ss/funcs_print.c
+++ ss/funcs_print.c
@@ -1,49 +1,6 @@
 #include "shell.h"
 
 /**
- * err_msg_cmd - writes an error msg to stderr
- * @argv: array of tokens
- * @n: count of commands
- *
- * Return: void
- */
-void err_msg_cmd(char **argv, int n)
-{
-	char *n_str;
-
-	n_str = n_conversion(n);
-
-	_puts_stderr("./hsh: ");
-	_puts_stderr(n_str);
-	_puts_stderr(": ");
-	_puts_stderr(argv[0]);
-	_puts_stderr(": not found\n");
-
-	free(n_str);
-}
-
-/**
- * err_msg_exit - writes an error msg to stderr
- * @status: input status of exit
- * @n: count of commands
- *
- * Return: void
- */
-void err_msg_exit(char *status, int n)
-{
-	char *n_str;
-
-	n_str = n_conversion(n);
-
-	_puts_stderr("./hsh: ");
-	_puts_stderr(n_str);
-	_puts_stderr(": exit: Illegal number: ");
-	_puts_stderr(status);
-	_puts_stderr("\n");
-
-	free(n_str);
-}
-/**
  * _putchar - writes the character c to stdout
  * @c: character to print
  *
