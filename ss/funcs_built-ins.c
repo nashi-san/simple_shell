f274794030d8be8f50e660822b4f8608c956c3e3
nashi-san <hamouda.ihsan@gmail.com>
2023-08-22 14:32:54 +0000

    customized setenv

--- ss/funcs_built-ins.c
+++ ss/funcs_built-ins.c
@@ -72,3 +72,60 @@ int exit_builtin(char *line, char **argv, int n, int *exit_status)
 	}
 	return (0);
 }
+/**
+ * setenv_builtin - Built-in function to set environment variable
+ * @line: command line (input)
+ * @argv: array of tokens
+ * @n: count of commands
+ * @exit_status: exit status
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int setenv_builtin(__attribute__((unused)) char *line, char **argv,
+		__attribute__((unused)) int n, int *exit_status)
+{
+	if (argv[1] == NULL || argv[2] == NULL)
+	{
+		*exit_status = 0;
+		return (0);
+	}
+	return (_setenv(argv[1], argv[2], exit_status));
+}
+/**
+ * _setenv - Sets the value of an environment variable
+ * @variable: The name of the variable
+ * @value: The value to set for the variable
+ * @exit_status: Pointer to the exit status variable
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int _setenv(char *variable, char *value, int *exit_status)
+{
+	int i;
+	char *new_var = malloc(strlen(variable) + strlen(value) + 2);
+
+	if (new_var == NULL)
+	{
+		*exit_status = 0;
+		return (0);
+	}
+
+	strcpy(new_var, variable);
+	strcat(new_var, "=");
+	strcat(new_var, value);
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (strncmp(environ[i], variable, strlen(variable)) == 0
+			&& environ[i][strlen(variable)] == '=')
+		{
+			environ[i] = new_var;
+			*exit_status = 1;
+			return (1);
+		}
+	}
+	environ[i] = new_var;
+	environ[i + 1] = NULL;
+	*exit_status = 1;
+	return (1);
+}
