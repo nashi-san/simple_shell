bf8c5f22032c60b4567bb30bc068cf48ff70e77e
nashi-san <hamouda.ihsan@gmail.com>
2023-08-22 18:17:32 +0000

    customizing all str functions

---
+++ ssv2/funcs_mem.c
@@ -0,0 +1,134 @@
+#include "shell.h"
+
+/**
+ * free_array - frees an array
+ * @argv: array of tokens
+ *
+ * Return: void
+ */
+void free_array(char **argv)
+{
+	int i = 0;
+
+	while (argv[i] != NULL)
+	{
+		free(argv[i]);
+		i++;
+	}
+	free(argv);
+}
+/**
+ * _memcpy - copy m bytes from m1 to m2
+ * @dest: 1st memory block
+ * @src: 2nd memory block
+ * @m: number of bytes to copy
+ *
+ * Return: pointer to memory block
+ */
+char *_memcpy(char *dest, const char *src, unsigned int m)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < m)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to previous memory block
+ * @old_size: size in bytes of previous memory block
+ * @new_size: size in bytes of new memory block
+ *
+ * Return: pointer to the new allocated memory, NULL on failure
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int min;
+	void *new_ptr;
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size < old_size)
+		min = new_size;
+	else
+		min = old_size;
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	new_ptr = _memcpy(new_ptr, ptr, min);
+	free(ptr);
+	return (new_ptr);
+}
+/**
+ * unsetenv_builtin - Built-in function to unset environment variable
+ * @line: command line (input)
+ * @argv: array of tokens
+ * @n: count of commands
+ * @exit_status: Pointer to the exit status
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int unsetenv_builtin(__attribute__((unused)) char *line, char **argv,
+		__attribute__((unused)) int n, int *exit_status)
+{
+	if (argv[1] == NULL)
+	{
+		*exit_status = 0;
+		return (0);
+	}
+
+	return (_unsetenv(argv[1], exit_status));
+}
+/**
+ * _unsetenv - Unset an environment variable
+ * @variable: Name of the environment variable to unset
+ * @exit_status: Pointer to the exit status
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int _unsetenv(char *variable, int *exit_status)
+{
+	int i, j;
+	int found = 0;
+	int variable_len = _strlen(variable);
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (strncmp(environ[i], variable, variable_len) == 0
+				&& environ[i][variable_len] == '=')
+		{
+			found = 1;
+			break;
+		}
+	}
+	if (found)
+	{
+		for (j = i; environ[j] != NULL; j++)
+		{
+			environ[j] = environ[j + 1];
+		}
+		*exit_status = 1;
+		return (1);
+	}
+	else
+	{
+		*exit_status = 0;
+		return (0);
+	}
+}
---
+++ ssv2/funcs_print.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to print
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts_stderr - writes a string to stderr
+ * @s: string
+ *
+ * Return: number of chars written, -1 on error
+ */
+int _puts_stderr(char *s)
+{
+	return (write(2, s, _strlen(s)));
+}
+
+/**
+ * _puts_stdout - prints a string to stdout
+ * @str: string
+ *
+ * Return: void
+ */
+void _puts_stdout(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
