b9a3aff12e188f9e0ad8fe26fe11d279d171c359
nashi-san <hamouda.ihsan@gmail.com>
2023-08-25 14:41:11 +0000

    fixing alias take2

--- my_alias2.c
+++ my_alias2.c
@@ -9,15 +9,27 @@
 char *find_alias(char *alias_name, ali_t **ali_list)
 {
 	ali_t *current = *ali_list;
+	ali_t *end_alias = NULL;
+	char *end_value = NULL;
+	char *the_end_value;
 
 	while (current != NULL)
 	{
 		if (_strcmp(current->name, alias_name) == 0)
 		{
-
-			return (current->value);
+			end_alias = current;
+			alias_name = current->value;
 		}
 		current = current->next;
 	}
-	return (NULL);
+
+	if (end_alias != NULL)
+	{
+		end_value = find_alias(alias_name, ali_list);
+		the_end_value = _strdup(end_value);
+		free(end_value);
+		return (the_end_value);
+	}
+
+	return _strdup(alias_name);
 }
